eb04fe92e2132621ecbeee10249349164f229dc2
aj17i <ahmad.y.jarouche06@gmail.com>
2023-12-24 17:41:41 +0200

    first commit

---
+++ pages/Login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Frontend Page</title>
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.css">
+
+</head>
+
+<body>
+    <div>
+        <h1 class="header">Hello to AJ's Health tracker</h1>
+
+        <a href="patientLogin.php" class="button"><img src="./css/images/icons8-patient-100.png" alt="" class="icon1">
+            Patient</a>
+
+        <a href="doctorLogin.php" class="button"><img src="./css/images/icons8-doctor-80.png" alt=""
+                class="icon">Doctor</a>
+
+        <div class="login-message">
+
+            <span>If you don't have an account, </span>
+            <a href="signup.php" class="signup-link">Sign up</a>
+            <span> (Only for patients!)</span>
+
+        </div>
+    </div>
+</body>
+
+</html>
+
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "healthtracker";
+$con = mysqli_connect($dbhost, $dbuser, $dbpass)
+    or die('Cannot connect to the server');
+mysqli_select_db($con, $dbname)
+    or die('DB selection problem');
+?>
