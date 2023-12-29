bca167830cd7f7ff9a665cc76624b71bb75c2257
aj17i <ahmad.y.jarouche06@gmail.com>
2023-12-29 22:16:05 +0200

    patient pages are all done

---
+++ pages/editpatient.php
@@ -0,0 +1,86 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "healthtracker";
+$con = mysqli_connect($dbhost, $dbuser, $dbpass)
+    or die('Cannot connect to the server');
+
+mysqli_select_db($con, $dbname)
+    or die('DB selection problem');
+
+error_reporting(E_ALL);
+
+session_start();
+
+if (!isset($_SESSION['logged']) || $_SESSION['logged'] !== true) {
+    header("Location: patient.php");
+    exit();
+}
+
+
+$user = $_SESSION['email'];
+
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fullname = isset($_POST['Fullname']) ? $_POST['Fullname'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $birth = isset($_POST['birthd']) ? $_POST['birthd'] : '';
+    $address = isset($_POST['address']) ? $_POST['address'] : '';
+    $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
+
+
+    $sql = "UPDATE patient SET ";
+    $updateFields = [];
+
+    if (!empty($fullname)) {
+        $updateFields[] = "Fullname='$fullname'";
+    }
+
+    if (!empty($phone)) {
+        $updateFields[] = "phone='$phone'";
+    }
+
+    if (!empty($birth)) {
+        $updateFields[] = "birthd='$birth'";
+    }
+
+    if (!empty($address)) {
+        $updateFields[] = "address='$address'";
+    }
+
+    if (!empty($gender)) {
+        $updateFields[] = "gender='$gender'";
+    }
+
+   
+    $sql .= implode(", ", $updateFields);
+
+
+    $sql .= " where email = '$user';";
+
+
+    mysqli_query($con, $sql);
+
+    if (mysqli_error($con)) {
+        die('Error in the query: ' . mysqli_error($con));
+    }
+}
+
+
+$nbrows = mysqli_affected_rows($con);
+
+if ($nbrows == 1) {
+    header("Location: patient.php");
+    exit(); // Redirect to the doctor.php page on success 
+} else {
+    echo "No rows were updated.";
+}
+
+mysqli_close($con);
+
+?>
--- pages/patient.php
+++ pages/patient.php
@@ -35,23 +35,42 @@ if (!$result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/p1.css">
 </head>
 
 <body>
-    <h1>Hello patient</h1>
-    <?php
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<div>";
-        echo "<h2>" . $row['Fullname'] .  "</h2>";
-        echo "<p>Email: " . $row['email'] . "</p>";
-        echo "<p>History: " . $row['history'] . "</p>";
-        echo "<p>Pid: " . $row['Pid'] . "</p>";
-        echo "<p>Age: " . $row['age'] . "</p>";
-        echo "</div>";
-    }
-    ?>
-    <form action="logout.php">
-        <button> Logout</button>
-    </form>
+    <div class="container">
+        <img src="./css/images/back8.jpg" alt="Image" />
+        <div class="text">
+            <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div>";
+                echo "<h2>" . $row['Fullname'] . "</h2>";
+                echo "<div class = 'detailes'><h3 style = 'font-weight = bold;'>Details:</h3>";
+                echo "<p>Pid: " . $row['Pid'] . "</p>";
+                echo "<p>Email: " . $row['email'] . "</p>";
+                echo "<p>History: " . $row['history'] . "</p>";
+                echo "<p>Medicine: " . $row['medicine'] . "</p>";
+                echo "<p>Allergies: " . $row['allergies'] . "</p>";
+                echo "<p>Gender: " . $row['gender'] . "</p>";
+                echo "<p>Birth Date: " . $row['birthd'] . "</p>";
+                echo "<p>Phone number: " . $row['phone'] . "</p>";
+                echo "<p>Age: " . $row['age'] . "</p></div>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+        <img class="circle-image" src="./css/images/patient.jpeg" alt="Circular Image" />
+        <div class="text">
+            <a href="editpatient.html" class="link">edit profile</a>
+            <br><br>
+            <form action="logout.php">
+                <button class="btn"> Logout</button>
+            </form>
+
+        </div>
+    </div>
 </body>
+
 </html>
